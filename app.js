452f039beee98caac01ac7a004b97e78c882740b
matthewdizon <dominic.dizon@obf.ateneo.edu>
2023-01-29 23:36:48 +0800

    added cors

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const getUsers = require("./routes/users");
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.get("/", (_req, res) => res.status(200).send("OK"));
 
